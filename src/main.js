1ab762855edfc8b5746e525eca644380634a3b80
Doyouyee <ffung435@gmail.com>
2024-11-06 21:32:00 +0800

    initial

---
+++ src/main.js
@@ -0,0 +1,11 @@
+function Register_page() {
+  window.location.href = 'Register.html'; // 跳轉到Register頁面
+}
+
+function Login_page() {
+  window.location.href = 'Login.html'; // 跳轉到Login頁面
+}
+
+function Home_page() {
+  window.location.href = 'Main page.html'; // 跳轉到Login頁面
+}
