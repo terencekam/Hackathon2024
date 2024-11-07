5714ea2f9279f9a0289885607030fda8f5c787c4
Doyouyee <ffung435@gmail.com>
2024-11-07 08:37:38 +0800

    initial

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+
+
 function Register_page() {
   window.location.href = 'register_page.html'; // 跳轉到Register頁面
 }
@@ -8,5 +10,19 @@ function Login_page() {
 
 function Home_page() {
   window.location.href = 'Main page.html'; // 跳轉到Login頁面
+}
+
+function format_check(){
+  let Email = document.getElementById('u13_input').value;
+  let Address = document.getElementById('u12_input').value;
+  let Company = document.getElementById('u15_input').value;
+  let Password = document.getElementById('u17_input').value;
+
+  if (Email.includes('@')) {
+    document.getElementById('myForm').submit();
+  }else if (Email.includes(' ') || Address.includes(' ') || Company.includes(' ') || Password.includes(' ')) {
+    window.location.href = 'register_page.html';
+    document.getElementById('error').innerHTML = "Something is going wrong... please try again.";
+  }
 }
 
