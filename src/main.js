60d275d5a310c654dfa1e1d6a18ef52f262439de
Doyouyee <ffung435@gmail.com>
2024-11-07 10:41:41 +0800

    initial

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 
 
-function Register_page() {
+function createAccount() {
   window.location.href = 'createAccount.html'; // 跳轉到Register頁面
 }
 
@@ -9,17 +9,18 @@ function Login_page() {
 }
 
 function Home_page() {
-  window.location.href = 'Main page.html'; // 跳轉到Login頁面
+  window.location.href = 'index.html'; // 跳轉到Login頁面
 }
 
 function format_check(){
-  let Email = document.getElementById('u13_input').value;
-  let Address = document.getElementById('u12_input').value;
-  let Company = document.getElementById('u15_input').value;
-  let Password = document.getElementById('u17_input').value;
+  let Email = document.getElementById('u13_input').textContent;
+  let Address = document.getElementById('u12_input').textContent;
+  let Company = document.getElementById('u15_input').textContent;
+  let Password = document.getElementById('u17_input').textContent;
 
   if (Email.includes('@')) {
     document.getElementById('myForm').submit();
+    window.location.href = 'login_page.html';
   }else if (Email.includes(' ') || Address.includes(' ') || Company.includes(' ') || Password.includes(' ')) {
     window.location.href = 'createAccount.html';
     document.getElementById('error').innerHTML = "Something is going wrong... please try again.";
